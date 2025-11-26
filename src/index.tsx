72ebcbcb795ecbbe67cd0c7ed4048df880f787b9
josefsmoreira <geralfalcaofibras@gmail.com>
2025-11-26 22:13:27 +0000

    up

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './index.css';
-import { LayoutProvider } from './contexts/LayoutContext';
-import { LanguageProvider } from './contexts/LanguageContext';
+import { App } from '../App';
+import '../index.css';
+import { LayoutProvider } from '../contexts/LayoutContext';
+import { LanguageProvider } from '../contexts/LanguageContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
