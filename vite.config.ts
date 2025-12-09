a6dd9c67c61a477b9dbe4161a21a3f009cda5d7c
josefsmoreira <geralfalcaofibras@gmail.com>
2025-12-09 17:03:57 +0000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,12 +9,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     define: {
-      // Expõe as variáveis de ambiente essenciais. 
-      // O uso de || '' garante que não passamos undefined para o JSON.stringify
+      // Expõe as variáveis de ambiente essenciais.
       'process.env.SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL || ''),
       'process.env.SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY || ''),
       'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY || ''),
-      'process.env.NODE_ENV': JSON.stringify(mode),
     },
     build: {
       chunkSizeWarningLimit: 2000,
