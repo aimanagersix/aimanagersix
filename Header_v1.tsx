5788842585ab5b57f3c990053a8add6231c260b9
josefsmoreira <geralfalcaofibras@gmail.com>
2025-12-19 20:01:55 +0000

    up

--- Header_v1.tsx
+++ Header_v1.tsx
@@ -1,14 +1,14 @@
 
 import React, { useState, useRef, useEffect } from 'react';
-import { Collaborator, UserRole, ModuleKey, PermissionAction } from './types';
-import { FaClipboardList, FaBuilding, FaUsers, FaDoorOpen as LogoutIcon, FaKey, FaBell, FaFingerprint, FaUserShield, FaDatabase, FaUserCircle, FaCalendarAlt, FaBook, FaQuestionCircle } from './components/common/Icons';
-import { FaShapes, FaTags, FaChartBar, FaTicketAlt, FaSitemap, FaNetworkWired, FaShieldAlt, FaBoxOpen, FaServer, FaLock, FaUnlock, FaColumns, FaRobot, FaTachometerAlt, FaAddressBook, FaCog, FaToolbox, FaChevronDown, FaBars, FaMapMarkedAlt, FaFileSignature, FaGraduationCap, FaShoppingCart, FaMobileAlt, FaUserTie } from 'react-icons/fa';
-import { useLanguage } from './contexts/LanguageContext';
-import { useLayout } from './contexts/LayoutContext';
-import MFASetupModal from './components/MFASetupModal';
-import AuditLogModal from './components/AuditLogModal';
-import DatabaseSchemaModal from './components/DatabaseSchemaModal';
-import InstallAppButton from './components/InstallAppButton';
+import { Collaborator, UserRole, ModuleKey, PermissionAction } from '../types';
+import { FaClipboardList, FaBuilding, FaUsers, FaDoorOpen as LogoutIcon, FaKey, FaBell, FaFingerprint, FaUserShield, FaDatabase, FaUserCircle, FaCalendarAlt, FaBook, FaQuestionCircle } from './common/Icons';
+import { FaShapes, FaTags, FaChartBar, FaTicketAlt, FaSitemap, FaSync, FaGlobe, FaNetworkWired, FaShieldAlt, FaBoxOpen, FaServer, FaLock, FaUnlock, FaColumns, FaRobot, FaTachometerAlt, FaAddressBook, FaCog, FaToolbox, FaChevronDown, FaBars, FaMapMarkedAlt, FaFileSignature, FaGraduationCap, FaShoppingCart, FaMobileAlt, FaUserTie } from 'react-icons/fa';
+import { useLanguage } from '../contexts/LanguageContext';
+import { useLayout } from '../contexts/LayoutContext';
+import MFASetupModal from './MFASetupModal';
+import AuditLogModal from './AuditLogModal';
+import DatabaseSchemaModal from './DatabaseSchemaModal';
+import InstallAppButton from './InstallAppButton';
 
 interface HeaderProps {
   currentUser: Collaborator | null;
