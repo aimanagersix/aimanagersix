d75ea822f914397a6f3f37e83fabe3a67766accb
josefsmoreira <geralfalcaofibras@gmail.com>
2025-12-21 21:33:50 +0000

    Delete Header_v1.tsx

--- Header_v1.tsx
+++
@@ -1,4 +0,0 @@
-
-// Este ficheiro foi movido para Header_v1.md para evitar erros de compilação TypeScript (TS2307).
-// O compilador 'tsc' ignora ficheiros .md mas tenta processar todos os .tsx.
-export {} 
